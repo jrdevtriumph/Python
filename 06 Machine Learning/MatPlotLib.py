97a57c0bd83f2bd1557935c6f9ff1e04c47c12a3
jrdevtriumph <iftekher@triumphdigital.co.th>
2024-04-29 16:28:44 +0600

    MatPlotLib Added

---
+++ 06 Machine Learning/MatPlotLib.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# Create some data
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+# Create a line plot
+plt.plot(x, y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('A Simple Line Plot')
+plt.show()
