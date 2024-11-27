52f941bd6d7e486eca5599f225aa5af71baabd7a
jrdevtriumph <iftekher@triumphdigital.co.th>
2024-11-27 14:37:43 +0600

    Documentation Started

--- 01 General/WhatsappBulkSMSSender.py
+++ 01 General/WhatsappBulkSMSSender.py
@@ -4,5 +4,6 @@ time.sleep(3)
 
 
 for i in range(50):
-    pyautogui.typewrite('Hello, This is for test')
+    # pyautogui.typewrite('Hello, This is for test')
+    pyautogui.typewrite('Happy Birthday Brother')
     pyautogui.press("enter")
--- 01 General/welcome.py
+++ 01 General/welcome.py
@@ -11,4 +11,4 @@ thisdict = {
   "model": "Mustang",
   "year": 1964
 }
-print(thisdict)
+print(thisdict["brand"])
